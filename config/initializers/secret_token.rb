ad76cade26ccb5d99d1291cddbd0e69104e5e561
linuxlady57 <rustinakirk@gmail.com>
2012-05-30 18:09:45 -0700

    Comic Book App Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComicApp::Application.config.secret_token = '480d6ee0b0b7af88ef10d47faa85b32d6b350128e872907849492fd29f01e5b7af081f633e5b6e6f605a58ee865e5250fa2667fc2c5c6e8531ddc51fa08a072a'
